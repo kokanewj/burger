b606d9d89c368ee5e545979721579583f7d2dbb3
Kuanysh Yersakhanov <yk_94@mail.ru>
2019-09-06 22:13:33 +0600

    Accordeon vertical/horizontal

--- main.js
+++ main.js
@@ -38,3 +38,65 @@ let FullScreenMenu = (function(options) {
 });
 
 FullScreenMenu.open(); //Открытие с помощью метода open();
+
+
+let teamItem = document.querySelectorAll('#team-accordeon__item');
+
+for (let i=0; i<teamItem.length; i++) {
+    teamItem[i].addEventListener("click", function(e){
+    e.preventDefault();
+    teamItem[i].classList.toggle("team-accordeon__item_active");
+    });
+}
+
+
+let menuItem = document.querySelectorAll('#menu-accordeon__item');
+
+for (let i=0; i<menuItem.length; i++) {
+    menuItem[i].addEventListener("click", function(e){
+    e.preventDefault();
+    menuItem[i].classList.toggle("menu-accordeon__item_active");
+    });
+}
+
+
+// let teamAccordeon = document.querySelector('.team-accordeon');
+    
+//     teamAccordeon.addEventListener('click', function(e) {
+//       e.preventDefault();
+
+//       let teamAccordeonItem = document.querySelectorAll('.team-accordeon__item');
+//       let target = e.target.closest('li');
+
+//       if (!target) return;
+
+//       if (!target.classList.contains('team-accordeon__item_active')) {
+//         for (let Item of teamAccordeonItem) {
+//           Item.classList.remove('team-accordeon__item_active');
+//         }
+//         target.classList.add('team-accordeon__item_active');
+//       } else {
+//         target.classList.remove('team-accordeon__item_active');
+//       }
+//     });
+
+
+//   let menuAccordeon = document.querySelector('.menu-accordeon');
+
+//     menuAccordeon.addEventListener('click', function(e) {
+//       e.preventDefault();
+
+//       let menuAccordeonItem = document.querySelectorAll('.menu-accordeon__item');
+//       let target = e.target.closest('li');
+
+//       if (!target) return;
+
+//       if (!target.classList.contains('menu-accordeon__item_active')) {
+//         for (let Item of menuAccordeonItem) {
+//           Item.classList.remove('menu-accordeon__item_active');
+//         }
+//         target.classList.add('menu-accordeon__item_active');
+//       } else {
+//         target.classList.remove('menu-accordeon__item_active');
+//       }
+//     });
