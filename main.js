0f1c14f9a33646bc267bf32421174cce48480ecd
Kuanysh Yersakhanov <yk_94@mail.ru>
2019-09-08 14:56:20 +0600

    Добавлено popup окно

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Полноэкранное меню
 let FullScreenMenu = (function(options) {
 
   let buttonOpen = document.querySelector(options.buttonOpen);
@@ -40,63 +41,227 @@ let FullScreenMenu = (function(options) {
 FullScreenMenu.open(); //Открытие с помощью метода open();
 
 
-let teamItem = document.querySelectorAll('#team-accordeon__item');
+// Тим аккордеон (при нажатии на следующий элемент, не закрывает активный)
 
-for (let i=0; i<teamItem.length; i++) {
-    teamItem[i].addEventListener("click", function(e){
-    e.preventDefault();
-    teamItem[i].classList.toggle("team-accordeon__item_active");
-    });
-}
+// let teamItem = document.querySelectorAll('#team-accordeon__item');
 
-
-let menuItem = document.querySelectorAll('#menu-accordeon__item');
-
-for (let i=0; i<menuItem.length; i++) {
-    menuItem[i].addEventListener("click", function(e){
-    e.preventDefault();
-    menuItem[i].classList.toggle("menu-accordeon__item_active");
-    });
-}
+// for (let i=0; i<teamItem.length; i++) {
+//     teamItem[i].addEventListener("click", function(e){
+//     e.preventDefault();
+//     teamItem[i].classList.toggle("team-accordeon__item_active");
+//     });
+// }
 
 
-// let teamAccordeon = document.querySelector('.team-accordeon');
+// Меню аккордеон (при нажатии на следующий элемент, не закрывает активный)
+// let menuItem = document.querySelectorAll('#menu-accordeon__item');
+
+// for (let i=0; i<menuItem.length; i++) {
+//     menuItem[i].addEventListener("click", function(e){
+//     e.preventDefault();
+//     menuItem[i].classList.toggle("menu-accordeon__item_active");
+//     });
+// }
+
+//Тим аккордеон
+let teamAccordeon = document.querySelector('.team-accordeon');
     
-//     teamAccordeon.addEventListener('click', function(e) {
-//       e.preventDefault();
+    teamAccordeon.addEventListener('click', function(e) {
+      e.preventDefault();
 
-//       let teamAccordeonItem = document.querySelectorAll('.team-accordeon__item');
-//       let target = e.target.closest('li');
+      let teamAccordeonItem = document.querySelectorAll('.team-accordeon__item');
+      let target = e.target.closest('li');
 
-//       if (!target) return;
+      if (!target) return;
 
-//       if (!target.classList.contains('team-accordeon__item_active')) {
-//         for (let Item of teamAccordeonItem) {
-//           Item.classList.remove('team-accordeon__item_active');
-//         }
-//         target.classList.add('team-accordeon__item_active');
-//       } else {
-//         target.classList.remove('team-accordeon__item_active');
-//       }
-//     });
+      if (!target.classList.contains('team-accordeon__item_active')) {
+        for (let Item of teamAccordeonItem) {
+          Item.classList.remove('team-accordeon__item_active');
+        }
+        target.classList.add('team-accordeon__item_active');
+      } else {
+        target.classList.remove('team-accordeon__item_active');
+      }
+    });
 
 
-//   let menuAccordeon = document.querySelector('.menu-accordeon');
+// Меню аккордеон (при нажатии на следующий элемент, не закрывает активный)
+  let menuAccordeon = document.querySelector('.menu-accordeon');
 
-//     menuAccordeon.addEventListener('click', function(e) {
-//       e.preventDefault();
+    menuAccordeon.addEventListener('click', function(e) {
+      e.preventDefault();
 
-//       let menuAccordeonItem = document.querySelectorAll('.menu-accordeon__item');
-//       let target = e.target.closest('li');
+      let menuAccordeonItem = document.querySelectorAll('.menu-accordeon__item');
+      let target = e.target.closest('li');
 
-//       if (!target) return;
+      if (!target) return;
 
-//       if (!target.classList.contains('menu-accordeon__item_active')) {
-//         for (let Item of menuAccordeonItem) {
-//           Item.classList.remove('menu-accordeon__item_active');
-//         }
-//         target.classList.add('menu-accordeon__item_active');
-//       } else {
-//         target.classList.remove('menu-accordeon__item_active');
-//       }
-//     });
+      if (!target.classList.contains('menu-accordeon__item_active')) {
+        for (let Item of menuAccordeonItem) {
+          Item.classList.remove('menu-accordeon__item_active');
+        }
+        target.classList.add('menu-accordeon__item_active');
+      } else {
+        target.classList.remove('menu-accordeon__item_active');
+      }
+    });
+
+
+//Слайдер
+const slide = (function(){
+  const left = document.querySelector('.slider__button_prev');
+  const right = document.querySelector('.slider__button_next');
+  const slider = document.querySelector('.slider__item');
+  const computed = getComputedStyle(slider);
+
+  let sliderWidth = parseInt(computed.width);
+
+  window.addEventListener('resize', function(){
+    currentRight = 0;
+    slider.style.right = currentRight;
+    sliderWidth = parseInt(computed.width);
+  }, true);
+
+  var sliderItemsCounter = slider.children.length;
+
+  let moveSlide = function(direction) {
+    direction.addEventListener("click", function(e) {
+      e.preventDefault();
+      let currentRight = parseInt(computed.right);
+
+      if (currentRight < (sliderItemsCounter-1)*sliderWidth && direction==right) {
+        slider.style.right = currentRight + sliderWidth + "px";
+      }
+
+      if (currentRight > 0 && direction==left) {
+        slider.style.right = currentRight - sliderWidth + "px";
+      }
+    });
+  }
+
+  let addListeners = function(){
+    moveSlide(right);
+    moveSlide(left);
+  }
+
+  return {init: addListeners}
+})();
+
+slide.init();
+
+
+//Форма отправки
+
+const myForm = document.querySelector('#myForm');
+const send =  document.querySelector('#send');
+
+send.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (validateForm(myForm)) {
+    const data = {
+      name: myForm.elements.name.value,
+      phone: myForm.elements.phone.value,
+      comment: myForm.elements.comment.value
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+    xhr.send(JSON.stringify(data));
+    xhr.addEventListener('load', () => {
+      if (xhr.response.status) {
+        console.log('Все ОК!');
+      }
+    });
+  }
+});
+
+function validateForm(form) {
+  let valid = true;
+
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.phone)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.comment)) {
+    valid = false;
+  }
+
+  return valid;
+}
+
+function validateField(field) {
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
+}
+
+//Modal window (по вебинару)
+
+// const openButton = document.querySelector(".review__btn");
+// const template = document.querySelector("#overlayTemplate").innerHTML;
+// const overlay = createOverlay(template);
+
+// openButton.addEventListener("click", function() {
+//   overlay.open();
+//   overlay.setContent("Спасибо, данные сохранены");
+// });
+
+// function createOverlay(template) {
+//   let fragment = document.createElement('div');
+
+//   fragment.innerHTML = template;
+
+//   const overlayElement = fragment.querySelector(".overlay");
+//   const contentElement = fragment.querySelector(".button_content");
+//   const closeElement = fragment.querySelector(".button_review_close");
+  
+//   fragment = null;
+
+//   overlayElement.addEventListener("click", e => {
+//     if (e.target === overlayElement) {
+//       closeElement.click();
+//     }
+//   });
+//   closeElement.addEventListener("click", () => {
+//     document.body.removeChild(overlayElement);
+//   });
+
+//   return {
+//     open() {
+//       document.body.appendChild(overlayElement);
+//     },
+//     close() {
+//       closeElement.click();
+//     },
+//     setContent(content) {
+//       contentElement.innerHTML = content;
+//     }
+//   };
+// }
+
+//Modal window
+const popupMenu = document.querySelector('.popup-menu');
+const reviewsList = document.querySelector('.reviews__list');
+const reviewClose = document.querySelector('.popup-menu__close');
+reviewClose.addEventListener('click', function(e) {
+  e.preventDefault();
+  popupMenu.style.display = 'none';
+  document.body.style.overflow = 'initial';
+});
+
+reviewsList.addEventListener('click', function(e) {
+  e.preventDefault();
+  var target = e.target.closest('button');
+  if (!target) return;
+    popupMenu.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+  });
+
+
+
+ 
