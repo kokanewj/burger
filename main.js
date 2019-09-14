601124abb8d88f4c221bde2ea6968e48745d1731
Kuanysh Yersakhanov <yk_94@mail.ru>
2019-09-14 22:40:02 +0600

    Slider, map and OnePageScroll

--- main.js
+++ main.js
@@ -41,28 +41,6 @@ let FullScreenMenu = (function(options) {
 FullScreenMenu.open(); //Открытие с помощью метода open();
 
 
-// Тим аккордеон (при нажатии на следующий элемент, не закрывает активный)
-
-// let teamItem = document.querySelectorAll('#team-accordeon__item');
-
-// for (let i=0; i<teamItem.length; i++) {
-//     teamItem[i].addEventListener("click", function(e){
-//     e.preventDefault();
-//     teamItem[i].classList.toggle("team-accordeon__item_active");
-//     });
-// }
-
-
-// Меню аккордеон (при нажатии на следующий элемент, не закрывает активный)
-// let menuItem = document.querySelectorAll('#menu-accordeon__item');
-
-// for (let i=0; i<menuItem.length; i++) {
-//     menuItem[i].addEventListener("click", function(e){
-//     e.preventDefault();
-//     menuItem[i].classList.toggle("menu-accordeon__item_active");
-//     });
-// }
-
 //Тим аккордеон
 let teamAccordeon = document.querySelector('.team-accordeon');
     
@@ -85,7 +63,7 @@ let teamAccordeon = document.querySelector('.team-accordeon');
     });
 
 
-// Меню аккордеон (при нажатии на следующий элемент, не закрывает активный)
+// Меню аккордеон
   let menuAccordeon = document.querySelector('.menu-accordeon');
 
     menuAccordeon.addEventListener('click', function(e) {
@@ -107,52 +85,52 @@ let teamAccordeon = document.querySelector('.team-accordeon');
     });
 
 
-//Слайдер
-const slide = (function(){
-  const left = document.querySelector('.slider__button_prev');
-  const right = document.querySelector('.slider__button_next');
-  const slider = document.querySelector('.slider_main');
-  const computed = getComputedStyle(slider);
+//Слайдер (предыдущий вариант)
+// const slide = (function(){
+//   const left = document.querySelector('.slider__button_prev');
+//   const right = document.querySelector('.slider__button_next');
+//   const slider = document.querySelector('.slider_main');
+//   const computed = getComputedStyle(slider);
 
-  let sliderWidth = parseInt(computed.width);
+//   let sliderWidth = parseInt(computed.width);
 
-  window.addEventListener('resize', function(){
-    currentRight = 0;
-    slider.style.right = currentRight;
-    sliderWidth = parseInt(computed.width);
-  }, true);
+//   window.addEventListener('resize', function(){
+//     currentRight = 0;
+//     slider.style.right = currentRight;
+//     sliderWidth = parseInt(computed.width);
+//   }, true);
 
-  var sliderItemsCounter = slider.children.length;
-  let flag = true;
+//   var sliderItemsCounter = slider.children.length;
+//   let flag = true;
 
-  let moveSlide = function(direction) {
-    direction.addEventListener("click", function(e) {
-      e.preventDefault();
-      let currentRight = parseInt(computed.right);
+//   let moveSlide = function(direction) {
+//     direction.addEventListener("click", function(e) {
+//       e.preventDefault();
+//       let currentRight = parseInt(computed.right);
 
-      if (currentRight < (sliderItemsCounter-1)*sliderWidth && direction==right && flag) {
-        flag = false;
-        slider.style.right = currentRight + sliderWidth + "px";
-        setTimeout(() => {flag = true}, 300)
-      }
+//       if (currentRight < (sliderItemsCounter-1)*sliderWidth && direction==right && flag) {
+//         flag = false;
+//         slider.style.right = currentRight + sliderWidth + "px";
+//         setTimeout(() => {flag = true}, 300)
+//       }
 
-      if (currentRight > 0 && direction==left && flag) {
-        flag = false;
-        slider.style.right = currentRight - sliderWidth + "px";
-        setTimeout(() => {flag = true}, 300)
-      }
-    });
-  }
+//       if (currentRight > 0 && direction==left && flag) {
+//         flag = false;
+//         slider.style.right = currentRight - sliderWidth + "px";
+//         setTimeout(() => {flag = true}, 300)
+//       }
+//     });
+//   }
 
-  let addListeners = function(){
-    moveSlide(right);
-    moveSlide(left);
-  }
+//   let addListeners = function(){
+//     moveSlide(right);
+//     moveSlide(left);
+//   }
 
-  return {init: addListeners}
-})();
+//   return {init: addListeners}
+// })();
 
-slide.init();
+// slide.init();
 
 
 //Форма отправки
@@ -214,51 +192,6 @@ function validateField(field) {
   return field.checkValidity();
 }
 
-
-//Modal window (по вебинару)
-
-// const openButton = document.querySelector(".review__btn");
-// const template = document.querySelector("#overlayTemplate").innerHTML;
-// const overlay = createOverlay(template);
-
-// openButton.addEventListener("click", function() {
-//   overlay.open();
-//   overlay.setContent("Спасибо, данные сохранены");
-// });
-
-// function createOverlay(template) {
-//   let fragment = document.createElement('div');
-
-//   fragment.innerHTML = template;
-
-//   const overlayElement = fragment.querySelector(".overlay");
-//   const contentElement = fragment.querySelector(".button_content");
-//   const closeElement = fragment.querySelector(".button_review_close");
-  
-//   fragment = null;
-
-//   overlayElement.addEventListener("click", e => {
-//     if (e.target === overlayElement) {
-//       closeElement.click();
-//     }
-//   });
-//   closeElement.addEventListener("click", () => {
-//     document.body.removeChild(overlayElement);
-//   });
-
-//   return {
-//     open() {
-//       document.body.appendChild(overlayElement);
-//     },
-//     close() {
-//       closeElement.click();
-//     },
-//     setContent(content) {
-//       contentElement.innerHTML = content;
-//     }
-//   };
-// }
-
 //Modal window
 const popupMenu = document.querySelector('.popup-menu');
 const reviewsList = document.querySelector('.reviews__list');
@@ -277,6 +210,258 @@ reviewsList.addEventListener('click', function(e) {
     document.body.style.overflow = 'hidden';
   });
 
+  // Yandex Map
+  ymaps.ready(init);
+
+  var placemarks = [
+      {
+          latitude: 59.97,
+          longitude: 30.31,
+          hintContent: '<div class="map__hint">ул. Литераторов, д. 19</div>',
+          balloonContent: [
+              '<div class="map__balloon">',
+              '<img class="map__burger-img" src="/img/icon/marker.png" alt="Бургер"/>',
+              'Самые вкусные бургеры у нас! Заходите по адресу: ул. Литераторов, д. 19',
+              '</div>'
+          ]
+      },
+      {
+          latitude: 59.94,
+          longitude: 30.25,
+          hintContent: '<div class="map__hint">Малый проспект В О, д 64</div>',
+          balloonContent: [
+              '<div class="map__balloon">',
+              '<img class="map__burger-img" src="/img/icon/marker.png" alt="Бургер"/>',
+              'Самые вкусные бургеры у нас! Заходите по адресу: Малый проспект В О, д 64',
+              '</div>'
+          ]
+      },
+      {
+          latitude: 59.93,
+          longitude: 30.34,
+          hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+          balloonContent: [
+              '<div class="map__balloon">',
+              '<img class="map__burger-img" src="/img/icon/marker.png" alt="Бургер"/>',
+              'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки, д. 56',
+              '</div>'
+          ]
+      }
+  ],
+      geoObjects= [];
+  
+  function init() {
+      var map = new ymaps.Map('map', {
+          center: [59.94, 30.32],
+          zoom: 12,
+          behaviors: ['drag']
+      });
+  
+      for (var i = 0; i < placemarks.length; i++) {
+              geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+              {
+                  hintContent: placemarks[i].hintContent,
+                  balloonContent: placemarks[i].balloonContent.join('')
+              },
+              {
+                  iconLayout: 'default#image',
+                  iconImageHref: '/img/icon/marker.png',
+                  iconImageSize: [46, 57],
+                  iconImageOffset: [-23, -57],
+                  iconImageClipRect: [[415, 0], [461, 57]]
+              });
+      }
+  
+      var clusterer = new ymaps.Clusterer({
+          clusterIcons: [
+              {
+                  href: '/img/icon/marker.png',
+                  size: [100, 100],
+                  offset: [-50, -50]
+              }
+          ],
+          clusterIconContentLayout: null
+      });
+  
+      map.geoObjects.add(clusterer);
+      clusterer.add(geoObjects);
+  }
+
+  //Composition
+
+$('.composition').mouseenter(function(){
+  $this = $(this);
+  $this.addClass('composition_active');
+
+  $('.composition__close').on('click',function(){
+      e.preventDefault();
+      $this.removeClass('composition_active');
+  });
+
+});
+
+$('.composition').mouseleave(function(){
+  $this = $(this);
+  $this.removeClass('composition_active');
+});
 
 
- 
+//jQuery новый слайдер
+var moveSlider = function (container, slideNum) {
+  var 
+      items = container.find('.slider__item'),
+      activeItem = items.filter('.slider__item_active'),
+      reqItem = items.eq(slideNum),
+      reqIndex = reqItem.index(),
+      list = container.find('.slider_main'),
+      duration = 100;
+
+  if (reqItem.length) {
+      list.animate({
+          'left' : -reqIndex*100 + '%'
+      }, duration, function(){
+          activeItem.removeClass('slider__item_active');
+          reqItem.addClass('slider__item_active');
+      });
+  }
+}
+
+$('.slider__button').on('click', function(e){
+  e.preventDefault();
+
+  var $this = $(this),
+      container = $this.closest('.slider-block'),
+      items = $('.slider__item', container),
+      activeItem = items.filter('.slider__item_active'),
+      existedItem, edgeItem, reqItem;
+
+  if ($this.hasClass('slider__button_next')) {
+      existedItem = activeItem.next();
+      edgeItem = items.first();
+  } 
+
+  if ($this.hasClass('slider__button_prev')) {
+      existedItem = activeItem.prev();
+      edgeItem = items.last();
+  }
+
+  reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
+
+  moveSlider(container, reqItem);
+});
+
+
+//pageScroll
+
+const sections = $(".section");  //сохраняем секцию в переменную
+const display = $("#content"); //то, что мы двигаем
+let inscroll = false; //скролится страничка или нет
+
+const mobdet = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobdet.mobile();
+
+const countPosition = sectionEq => {
+  return `${sectionEq * -100}%`;
+};
+
+const switchActiveClass = (elems, elemEq) => {
+  elems
+    .eq(elemEq)
+    .addClass("active") //добавление класса к отображаемой секции
+    .siblings()
+    .removeClass("active"); //удаление класса
+};
+
+const unBlockScroll = () => {
+  setTimeout(() => {
+    inscroll = false;
+  }, 1000 + 300); // Время transition + время за которое закончится инерции на тач-устройствах
+};
+
+const performTransition = sectionEq => {  //SectionEq отображает элемент по номеру
+  if (inscroll) return;
+  inscroll = true;
+
+  const position = countPosition(sectionEq);  //расчитываем позицию
+  const switchFixedMenuClass = () =>
+    switchActiveClass($(".fixed-menu__item"), sectionEq);
+
+  switchActiveClass(sections, sectionEq);
+  switchFixedMenuClass();
+
+  display.css({
+    transform: `translateY(${position})`   //затрагивает только Composite, анимация будет плавной
+  });
+
+  unBlockScroll();
+};
+
+const scrollViewport = direction => {
+  const activeSection = sections.filter(".active");   // выбираем секцию с классом active
+  const nextSection = activeSection.next();           // следующая секция
+  const prevSection = activeSection.prev();           // предыдущая секция
+
+  if (direction === "next" && nextSection.length) {      //проверяем направление и существование секции
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {      //проверяем направление и существование секции
+    performTransition(prevSection.index());
+  }
+};
+
+$(document).on({
+  wheel: e => {                               //значение, которое отслеживает скролл по колесику мыши
+    const deltaY = e.originalEvent.deltaY;
+    if (deltaY < 0) {
+      scrollViewport("prev");
+    }
+    if (deltaY > 0) {
+      scrollViewport("next");
+    }
+  },
+    //делаем так, чтобы при вводе текста у нас не уходил вниз или вверх   
+  keydown: e => {                              
+    const tagName = e.target.tagName.toLowerCase();
+    const textareaInputs = tagName === "input" || tagName === "textarea";
+
+    if (textareaInputs) return;
+
+    switch (e.keyCode) {
+      case 38:                 // предыдущая секция
+        scrollViewport("prev");
+        break;
+
+        case 40:              // следуюшая секция
+          scrollViewport("next");
+          break;
+    }
+  }
+});
+
+$("[data-scroll-to]").on("click", e => {
+  e.preventDefault();
+  performTransition(parseInt($(e.currentTarget).attr("data-scroll-to")));
+});
+
+//  swipe на мобильных устройствах
+if (isMobile) {             
+  window.addEventListener(
+    "touchmove",
+    e => {
+      e.preventDefault();
+    },
+    { passive: false }
+  );
+
+  $("body").swipe({
+    swipe: (event, direction) => {
+      let scrollDirecrion;
+      if (direction === "up") scrollDirecrion = "next";
+      if (direction === "down") scrollDirecrion = "prev";
+      scrollViewport(scrollDirecrion);
+    }
+  });
+}
+
+
